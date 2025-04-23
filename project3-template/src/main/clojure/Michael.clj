16a2d5ecb517803adaebe1eb9ea07ee28ed1457b
mwd00008 <mwd00008@mix.wvu.edu>
2025-04-23 16:16:39 -0400

    Added and completed Michael.java and Michael.clj

---
+++ project3-template/src/main/clojure/Michael.clj
@@ -0,0 +1,19 @@
+(ns Alice)
+
+(defn map [fun lizt]
+	(cond
+		(empty?lizt) '()
+		:else (cons (fun (first lizt))
+			(map fun (rest lizt)))
+		)
+)
+
+(defn same [lizt1 lizt2]
+	(cond
+		(empty? lizt1) (empty? lizt2)
+		(empty? lizt2) false
+		(= (first lizt1) (first lizt2))
+			(same (rest lizt1) (rest lizt2))
+		:else false
+	)
+)
---
+++ project3-template/src/main/java/Michael.java
@@ -0,0 +1,15 @@
+public static <T, R> List<R> map(Function<T, R> fun, List<T> list) {
+	if (list.isEmpty()) {
+		return new ArrayList<R>();
+	} else {
+		List<R> returnVar = new ArrayList<R>();
+		for (T member : list) {
+			returnVar.add(fun.apply(member));
+		}
+		return returnVar;
+	}
+}
+
+public static <T, R> boolean same(List<R> list1, List<T> list2) {
+	return list1.equals(list2);
+}
