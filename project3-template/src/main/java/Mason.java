8fcc5d7189182f0a3da968f4798f9964d6915b6a
mp4dev <mcp00014@mix.wvu.edu>
2025-04-29 11:18:19 -0400

    Started Mason.java

---
+++ project3-template/src/main/java/Mason.java
@@ -0,0 +1,21 @@
+import  java.util.List;
+import  java.util.ArrayList;
+
+// Define function called intersect that takes in 2 lists
+// Returns a list
+public class Mason {
+    public static <T> List<T> intersect(List<T> list1, List<T> list2) {
+        // if list1 is null, return empty list
+        if (list1 == null) {
+            return new ArrayList<T>();
+        }
+        // (memq (car lizt1) list2)
+        //     (cons (car lizt1) (intersect (cdr lizt1) lizt2))
+        
+
+        else // return intersect passed cdr of list
+    }
+}
+
+
+// 
